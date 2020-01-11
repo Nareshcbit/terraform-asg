762c69adb877602583f403eb25ec365c9f5b8e3b
Naresh Madiraju <naresh.johnny@gmail.com>
2020-01-11 17:55:05 -0500

    added fields for launch configuration

--- main.tf
+++ main.tf
@@ -1,3 +1,22 @@
+data "aws_ami" "latest-ubuntu" {
+most_recent = true
+owners = ["099720109477"] # Canonical
+
+  filter {
+      name   = "name"
+      values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+  }
+
+  filter {
+      name   = "virtualization-type"
+      values = ["hvm"]
+  }
+}
+
+
+
 resource "aws_launch_configuration" "this" {
-  name = "${var.launch_config_name}"
+  name          = "${var.launch_config_name}"
+  image_id      = "${data.aws_ami.ubuntu.id}"
+  instance_type = "t2.micro"
 }
--- variables.tf
+++ variables.tf
@@ -1,1 +1,4 @@
 variable "launch_config_name" { default = "launch_config"}
+
+variable "image_id" { default = "launch_config"}
+variable "instance_type" { default = "t2.micro"}
